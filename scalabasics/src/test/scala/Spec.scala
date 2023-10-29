c8876e9bbe0e23854ed67d8a73bcf0cce592d46e
Tagir <tagir2911@mail.ru>
2023-10-30 00:11:46 +0300

    init new proj

--- scalabasics/src/test/scala/Spec.scala
+++ scalabasics/src/test/scala/Spec.scala
@@ -1,9 +1,13 @@
+import lectures.week1basics.Functions._
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Spec extends AnyFlatSpec with Matchers {
-  "smth" should "" in {
-    42 shouldEqual 42
+  "aPerson" should "return name and surname in correct format" in {
+    aPerson(
+      "Tagir",
+      "Khaybullin"
+    ) shouldEqual "Hello, Tagir Khaybullin, nice to meet you in Scala!"
   }
 }
