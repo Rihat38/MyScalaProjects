235debb9cbc06737c31fc0b8eb2043cbd4d99646
Tagir <tagir2911@mail.ru>
2023-11-09 00:46:33 +0300

    +blocks 2.6 - 3.5

--- scalabasics/src/main/scala/lectures/week2oop/Generics.scala
+++ scalabasics/src/main/scala/lectures/week2oop/Generics.scala
@@ -1,5 +1,34 @@
 package lectures.week2oop
 
 object Generics {
+  def countSumOfInt(nums: List[Int]): Int = nums.sum
+  countSumOfInt(List(1, 2, 3)) //--> 6
+  /*
+  That's a bad implementation, because if we will want to expand
+  our method for double, float or something else, we will have to
+  duplicate code for another type. Instead of this we can use a
+  generics
+  -------------------
+  countSumOfInt(List(1.3, 2.2, 3.1)) --> ERROR
+   */
 
+  def countSum[A: Numeric](nums: List[A]): A = nums.sum
+
+  println(countSum[Int](List(1, 2, 3))) //--> 6
+  println(countSum[Double](List(1.3, 2.2, 3.5))) //--> 7.0
+
+  def createElement[A](el: A): A = {
+    el
+  }
+
+  val anInt: Int = createElement(2)
+  val aString: String = createElement("some string")
+  println(s"anInt = $anInt, aString = $aString") //--> anInt = 2, aString = some string
+
+
+  case class Vehicle(name: String)
+
+  val aVehicle: Vehicle = createElement(Vehicle("car"))
+
+  println(s"aVehicle = $aVehicle, Vehicle.name = ${aVehicle.name}") //--> aVehicle = Vehicle(car), Vehicle.name = car
 }
--- scalabasics/src/main/scala/lectures/week2oop/ObjectsOOP.scala
+++ scalabasics/src/main/scala/lectures/week2oop/ObjectsOOP.scala
@@ -1,5 +1,47 @@
 package lectures.week2oop
 
 object ObjectsOOP {
+  class MyString(val str: String) {
+    private var extra = "extraData"
 
+    /*
+    Using override def toString we have an opportunity to configure a
+    string representation of our class
+    -------------------------
+    def getString: String = str + extra
+    println(MyString.apply("welcome").getString) --> welcomeextraData
+    -------------------------
+    override def toString: String = str + extra
+    println(MyString("welcome")) --> welcomeextraData
+    -------------------------
+     */
+    override def toString: String = str + extra
+  }
+
+  object MyString {
+    def apply(base: String, extras: String): MyString = {
+      val s = new MyString(base)
+      s.extra = extras
+      s
+    }
+
+    def apply(base: String) = new MyString(base)
+  }
+
+  class Number(val num: Int)
+
+  object Number {
+    val Pi = 3.14
+
+    /*Using factory method we can add an additional functionality to class constructor.
+    After that we can sum two Numbers using a construction like constructor.
+    -------------------------
+    val numA = new Number(1)
+    val numB = new Number(2)
+    val numC = Number(numA, numB)
+
+    println(numC.num) --> 3
+    -------------------------*/
+    def apply(x: Number, y: Number): Number = new Number(x.num + y.num)
+  }
 }
